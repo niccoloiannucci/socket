ff6da7dd75aafc288c00882cd26200ef4aaf6f6a
niccoloiannucci <92153350+niccoloiannucci@users.noreply.github.com>
2022-02-10 12:42:13 +0100

    calcolatrice versione 2 non commentata

---
+++ calcoClient2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import json
+import socket
+
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 22224
+
+def invia_comandi(sock_service):
+    while True:
+        primoNumero=input("Inserisci il primo numero exit() per uscire")
+        if primoNumero=="exit()":
+            break
+        primoNumero=float(primoNumero)
+        operazione=input("Inserisci l'operazione (+,-,*,/,%)")
+        secondoNumero=float(input("Inserisci il secondo numero"))
+        messaggio={'primoNumero':primoNumero, 
+        'operazione':operazione, 
+        'secondoNumero':secondoNumero}
+        messaggio=json.dumps(messaggio) #trasforma l'oggetto in una stringa
+        sock_service.sendall(messaggio.encode("UTF-8"))
+        data=sock_service.recv(1024)
+        print("Risultato: ", data.decode())
+
+def connessione_server(SERVER_ADDRESS,SERVER_PORT):
+    sock_service = socket.socket()
+    sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+    print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+    invia_comandi(sock_service)
+
+if __name__=='__main__':
+    connessione_server(SERVER_ADDRESS,SERVER_PORT)
